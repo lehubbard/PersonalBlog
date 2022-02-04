bd1d6b0f51dec10af0e6a1b3a22dcd985caab723
Lucas <lehubbard@protonmail.com>
2022-02-04 12:57:08 -0500

    Added Gatsby

---
+++ gatsby/personal-site/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer() {
+    return ( 
+        <footer>
+        <nav class="socialIcons">
+            <ul>
+                <li><link href="#">Instagram</a></li>
+                <li><img src="img/arrow.png" alt="arrow icon" class="socialArrow" /></li>
+                <li><a href="#">  Linkedin</a></li>
+                <li><img src="img/arrow.png" alt="arrow icon" class="socialArrow" /></li>
+                <li><a href="#">  Twitter</a></li>
+                <li><img src="img/arrow.png" alt="arrow icon" class="socialArrow" /></li>
+            </ul>
+        </nav>
+    </footer>
+     );
+}
+
+export default Footer;
---
+++ gatsby/personal-site/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PageNotFound() {
+    return ( 
+        <h1>Can't find what you're looking for!</h1>
+     );
+}
+
+export default PageNotFound;
