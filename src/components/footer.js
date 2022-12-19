3238c8659f6ee25c97af62a8176082f062eb80fd
Lucas Hubbard <lehubbard@protonmail.com>
2022-12-19 08:24:15 -0500

    updated styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,13 +7,13 @@ function Footer() {
         <nav className="socialIcons">
             <ul>
                 <li><a href="https://www.instagram.com/lucas_hubbard/" target="_blank">Instagram</a></li>
-                <li><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
+                <li class="footer-arrow"><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
                 <li><a href="https://www.linkedin.com/in/lucashubbard22" target="_blank">  Linkedin</a></li>
-                <li><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
+                <li class="footer-arrow"><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
                 <li><a href="https://github.com/lehubbard" target="_blank">  Github</a></li>
-                <li><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
-                <li><a href="https://www.youtube.com/user/lucashubbard1" target="_blank">  Youtube</a></li>
-                <li><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
+                <li class="footer-arrow"><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
+                <li ><a href="https://www.youtube.com/user/lucashubbard1" target="_blank">  Youtube</a></li>
+                <li class="footer-arrow"><StaticImage src="../images/arrow.png" alt="arrow icon" className="socialArrow"/></li>
             </ul>
         </nav>
     </footer>
